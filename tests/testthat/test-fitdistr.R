502692d99ef328f602951d22df1eda02da4be30a
mbedward <michael.bedward@gmail.com>
2025-09-23 17:03:36 +1000

    tweak tests

--- tests/testthat/test-fitdistr.R
+++ tests/testthat/test-fitdistr.R
@@ -1,33 +1,37 @@
 test_that("observed proportion of zeros is returned", {
-  true_pzero <- seq(0, 1, 0.1)
   Nx <- 100
+  Nzeros <- seq(2, Nx-1, by=5)
+  true_pzero <- Nzeros / Nx
 
-  for (pzero in true_pzero) {
-    x <- runif(Nx)
-    is_zero <- as.logical( rbinom(Nx, 1, pzero) )
+  estimated_pzero <- sapply(Nzeros, function(n) {
+    x <- rbeta(Nx, 2, 2)
 
-    x[is_zero] <- 0
-    obs_pzero <- mean(is_zero)
+    # Set some values to 0
+    x[sample(1:Nx, size=n)] <- 0
 
     res <- fit_zoabeta(x)
-    expect_equal(res['pzero'], obs_pzero, ignore_attr = "names")
-  }
+    res['pzero']
+  })
+
+  expect_true(all.equal(estimated_pzero, true_pzero, check.attributes = FALSE))
 })
 
 test_that("observed proportion of ones is returned", {
-  true_pone <- seq(0, 1, 0.1)
   Nx <- 100
+  Nones <- seq(2, Nx-1, by=5)
+  true_pone <- Nones / Nx
 
-  for (pone in true_pone) {
-    x <- runif(Nx)
-    is_one <- as.logical( rbinom(Nx, 1, pone) )
+  estimated_pone <- sapply(Nones, function(n) {
+    x <- rbeta(Nx, 2, 2)
 
-    x[is_one] <- 1
-    obs_pone <- mean(is_one)
+    # Set some values to 1
+    x[sample(1:Nx, size=n)] <- 1
 
     res <- fit_zoabeta(x)
-    expect_equal(res['pone'], obs_pone, ignore_attr = "names")
-  }
+    res['pone']
+  })
+
+  expect_true(all.equal(estimated_pone, true_pone, check.attributes = FALSE))
 })
 
 test_that("shape params are NA when data have too many zeros", {
@@ -44,6 +48,7 @@ test_that("shape params are NA when data have too many ones", {
   expect_true(is.na(res['shape2']))
 })
 
+# TODO: this about a more robust way to test the fitting method
 test_that("beta shape params are estimated reliably", {
   true_shapes <- expand.grid(
     shape1 = seq(1, 10, 1.5),
@@ -53,6 +58,9 @@ test_that("beta shape params are estimated reliably", {
 
   Nx <- 1e3
 
+  # Simulate beta values for each set of true shape parameters and then use
+  # fit_zoabeta to try to recover the input parameters. Express agreement as the
+  # ratio of estimated to true parameters.
   param_ratio <- apply(true_shapes, MARGIN = 1, FUN = function(shapes) {
     x <- rbeta(Nx, shapes[1], shapes[2])
     res <- fit_zoabeta(x)
@@ -61,8 +69,7 @@ test_that("beta shape params are estimated reliably", {
     res[c('shape1', 'shape2')] / shapes
   })
 
-  # Check that the ratios are reasonably close to 1.
-  # TODO - think about a more robust way to implement this test
+  # Allow some wiggle room between true and estimated parameters
   testthat::expect_true(all(param_ratio > 0.8 & param_ratio < 1.2))
 })
 
